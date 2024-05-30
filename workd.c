782902d98b11432e9bee01498204c33ab9d0bfe3
Siva Nageswara Rao.V <siva.v@yoctotutor.com>
2024-05-30 11:23:51 +0530

    add new prints in world c file
    Signed-off-by: Siva Nageswara Rao.V <siva.v@yoctotutor.com>

--- workd.c
+++ workd.c
@@ -2,5 +2,10 @@
 int main()
 {
 	printf("hello world \n");
+	printf("hello world \n");
+	printf("hello world \n");
+	printf("hello world \n");
+	printf("hello world \n");
+	printf("hello world \n");
 	return 0;
 }
